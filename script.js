6743628234b7979910431ebd1e018da4326aa1f2
JaffertheCoder <jafaralinh313@gmail.com>
2026-08-26 10:52:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let timeLeft = 25 * 60;
 let timerDisplay = document.getElementById("timer");
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+
+    timerDisplay.textContent =
+        String(minutes).padStart(2, "0") + ":" +
+        String(seconds).padStart(2, "0");
+}
 let timeLeft = 25 * 60;
 let timer;
 
